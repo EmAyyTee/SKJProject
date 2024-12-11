e8692323b889512fbc8e13039acca85c4e3a79e9
EmAyyTee <hanzoswitchmail@gmail.com>
2024-12-11 19:35:46 +0100

    Added a user functionality to decide what to do with the app

--- s32097_skj_project/src/Main.java
+++ s32097_skj_project/src/Main.java
@@ -1,23 +1,34 @@
 import java.io.IOException;
 import java.net.BindException;
+import java.util.Scanner;
 import java.util.concurrent.TimeUnit;
 
 public class Main {
     public static void main(String[] args) throws InterruptedException {
-//        TestCases.caseTesting();
 
-        //Master in case slaves are gonna be coming from somewhere else
+        System.out.println("Tell me what to do:\n1) Run test cases\n2)Run the server");
+        Scanner sc = new Scanner(System.in);
 
-        int masterPort = 12345;
-        Thread masterThread = new Thread(() -> {
-            try {
-                DAS master = new DAS(masterPort, 10);
-                master.start();
-            } catch (IOException e) {
-                e.printStackTrace();
+        int answerFromUser = sc.nextInt();
+
+        switch(answerFromUser) {
+            case 1 -> TestCases.caseTesting();
+            case 2 -> {
+                int masterPort = 12345;
+                Thread masterThread = new Thread(() -> {
+                    try {
+                        DAS master = new DAS(masterPort, 10);
+                        master.start();
+                    } catch (IOException e) {
+                        e.printStackTrace();
+                    }
+                });
+                masterThread.start();
+                TimeUnit.SECONDS.sleep(1);
             }
-        });
-        masterThread.start();
-        TimeUnit.SECONDS.sleep(1);
+            default -> {
+                System.out.println("Invalid input");
+            }
+        }
     }
 }
